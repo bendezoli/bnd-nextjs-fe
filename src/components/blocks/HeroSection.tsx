8bfa99f5a60ddcb5276de6439ee1970b07d0f3d8
Zoli Bende <zoltan@octopus-digital.pro>
2026-02-17 12:56:54 +0200

    some hero tweaks

--- src/components/blocks/HeroSection.tsx
+++ src/components/blocks/HeroSection.tsx
@@ -20,22 +20,24 @@ export function HeroSection({
           alt={
             backgroundImage.alternativeText || "No alternative text provided"
           }
-          className="hero__background-image"
+          className=""
         />
-        {darken && <div className="hero__background__overlay"></div>}
+        {darken && <div className="absolute inset-0 bg-black/50"></div>}
       </div>
-      <div className={`hero__heading hero__heading--`}>
-        <h1>{heading}</h1>
-        {author && <p className="hero__author">{author}</p>}
-        {publishedAt && <p className="hero__published-at">{publishedAt}</p>}
+      <div className="hero-text-wrapper absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-center text-white px-4">
+        <div className={`hero__heading hero__heading--`}>
+          <h1>{heading}</h1>
+          {author && <p className="hero__author">{author}</p>}
+          {publishedAt && <p className="hero__published-at">{publishedAt}</p>}
+        </div>
+        {cta && (
+          <button className={`btn btn--medium btn--`}>
+            <Link href={cta.href} target={cta.isExternal ? "_blank" : "_self"}>
+              {cta.text}
+            </Link>
+          </button>
+        )}
       </div>
-      {cta && (
-        <button className={`btn btn--medium btn--`}>
-          <Link href={cta.href} target={cta.isExternal ? "_blank" : "_self"}>
-            {cta.text}
-          </Link>
-        </button>
-      )}
     </section>
   );
 }
